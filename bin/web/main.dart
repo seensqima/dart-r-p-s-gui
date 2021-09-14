0d69897d2e244aa852c2ed931854c366675fc794
Ta-Seen Tahmid <tasinahmed5800@gmail.com>
2021-09-14 10:54:12 +0600

    Update main.dart

--- bin/web/main.dart
+++ bin/web/main.dart
@@ -11,17 +11,21 @@ void main() {
   final scissor_icon = querySelector('.scissor');
 
   querySelector('#rock')?.onClick.listen((_) {
+    
     final ai_move = object.Method();
+    
     if (ai_move == 'r') {
       player_display?.text = 'You : ${rock_icon?.text}';
       ai_display?.text = 'AI : ${rock_icon?.text}';
       object.Draw();
     }
+    
     else if (ai_move == 's') {
       player_display?.text = 'You : ${rock_icon?.text}';
       ai_display?.text = 'AI : ${scissor_icon?.text}';
       object.Win();
     }
+    
     else {
       player_display?.text = 'You : ${rock_icon?.text}';
       ai_display?.text = 'AI : ${paper_icon?.text}';
@@ -30,17 +34,21 @@ void main() {
   });
 
   querySelector('#paper')?.onClick.listen((_) {
+    
     final ai_move = object.Method();
+    
     if (ai_move == 'p') {
       player_display?.text = 'You : ${paper_icon?.text}';
       ai_display?.text = 'AI : ${paper_icon?.text}';
       object.Draw();
     }
+    
     else if (ai_move == 'r') {
       player_display?.text = 'You : ${paper_icon?.text}';
       ai_display?.text = 'AI : ${rock_icon?.text}';
       object.Win();
     }
+    
     else {
       player_display?.text = 'You : ${paper_icon?.text}';
       ai_display?.text = 'AI : ${scissor_icon?.text}';
@@ -49,17 +57,21 @@ void main() {
   });
 
   querySelector('#scissor')?.onClick.listen((_) {
+    
     final ai_move = object.Method();
+    
     if (ai_move == 's') {
       player_display?.text = 'You : ${scissor_icon?.text}';
       ai_display?.text = 'AI : ${scissor_icon?.text}';
       object.Win();
     }
+    
     else if (ai_move == 'p') {
       player_display?.text = 'You : ${scissor_icon?.text}';
       ai_display?.text = 'AI : ${paper_icon?.text}';
       object.Draw();
     }
+    
     else {
       player_display?.text = 'You : ${scissor_icon?.text}';
       ai_display?.text = 'AI : ${rock_icon?.text}';
@@ -69,7 +81,9 @@ void main() {
 }
 
 class classname {
+  
   final screen = querySelector('#output');
+  
   void Lose() {
     screen?.text = 'You Lose!';
     screen?.style.color = 'red';
@@ -86,18 +100,23 @@ class classname {
   }
 
   String Method() {
+    
     final random_number = Random();
     final ai_random_move = random_number.nextInt(3) + 1;
     var ai_move = '';
+    
     if (ai_random_move == 1) {
       ai_move = 'r';
     }
+    
     else if (ai_random_move == 2) {
       ai_move = 'p';
     }
+    
     else if (ai_random_move == 3) {
       ai_move = 's';
     }
+    
     return ai_move;
   }
 }
